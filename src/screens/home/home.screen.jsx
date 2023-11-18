e11719f23d68007c64a077cc043fb0fe9a8c59d0
Abdul Rashid <ar1242112@gmail.com>
2023-11-19 00:48:40 +0300

    fix styling

--- src/screens/home/home.screen.jsx
+++ src/screens/home/home.screen.jsx
@@ -293,7 +293,7 @@ export const HomeScreen = () => {
         style={{ width: "100%", height: "100vh" }}
       >
         <div
-          style={{ position: "absolute", top: "10px", left: "10px", zIndex: 1 }}>
+          style={{ position: "absolute", top: "76px", left: "6px",right:"6px", zIndex: 1 }}>
           <QuestCard />
         </div>
         <div>
--- src/screens/home/home.styles.ts
+++ src/screens/home/home.styles.ts
@@ -9,7 +9,9 @@ export const HomeContainer = styled.div`
   display:flex;
   align-items:center;
   justify-content:space-between;
-  
+  background:#2B83F6;
+  color: #F3322C;
+  // opacity: 0.8
 
 }
 h1{
